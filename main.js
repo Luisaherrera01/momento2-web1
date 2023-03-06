6249db858f5e9569f4995733fdaa3f3206aad552
sebascerpo <409@academicos.cesde.edu.co>
2023-03-06 11:49:17 -0500

    se crea el proyecto

---
+++ main.js
@@ -0,0 +1,14 @@
+//inicio de sesion
+let nombre = document.querySelector("#nombre").textContent="luisa";
+    let contraseña = document.querySelector("#contraseña").textContent= "123";
+
+
+
+const btnEnviar =document.querySelector("#btnenviar")
+
+
+const inicioSesion = () =>{
+    window.open("index.html","_blank");
+}
+
+btnEnviar.addEventListener('click',)
